99273ae25f52689f9b4bf23eb7b8440d6fd2aee1
Mandalore477 <43152684+Mandalore477@users.noreply.github.com>
2019-08-09 14:14:10 -0400

    Added UI, Score system, level progression

---
+++ JohnChick/Assets/Scripts/HighlightController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightController : MonoBehaviour
+{
+    private HighlightObject highlightObj;
+
+    public void SelectObj(HighlightObject pHighlightObj)
+    {
+        if(highlightObj != null)
+        {
+            highlightObj.StopHighlight();
+        }
+        highlightObj = pHighlightObj;
+        highlightObj.StartHighlight();
+
+    }
+
+}
