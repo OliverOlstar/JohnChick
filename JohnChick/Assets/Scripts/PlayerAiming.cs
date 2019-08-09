339d23212bb03df4bed6fd9823f1723cc598b67f
Oliver <Olstar_123@users.noreply.github.com>
2019-08-08 23:40:58 -0400

    Added Project Back in

---
+++ JohnChick/Assets/Scripts/PlayerAiming.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAiming : MonoBehaviour
+{
+    private Shooting _shooting;
+
+    void Start()
+    {
+        _shooting = GetComponent<Shooting>();
+    }
+    
+    void Update()
+    {
+        Rotation();
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _shooting.StartShooting();
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            _shooting.StopShooting();
+        }
+    }
+
+    void Rotation()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - transform.position;
+            float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+    }
+}
---
+++ JohnChick/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float maxSpeed = 5f;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float rayDistance = 0.2f;
+
+    private Rigidbody _rb;
+
+    private bool OnGround = true;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        OnGroundCheck();
+        Jump();
+        Move();
+    }
+
+    void OnGroundCheck()
+    {
+        OnGround = (Physics.Raycast(transform.position, Vector3.down, rayDistance));
+    }
+
+    void Move()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveInput = moveInput.normalized * moveSpeed * Time.deltaTime;
+
+        if (_rb.velocity.magnitude < maxSpeed)
+            _rb.AddForce(moveInput, ForceMode.VelocityChange);
+    }
+
+    void Jump()
+    {
+        if (OnGround && Input.GetButtonDown("Jump"))
+            _rb.AddForce(Vector3.up * jumpForce);
+    }
+}
