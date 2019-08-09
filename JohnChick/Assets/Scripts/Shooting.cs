3341127ab322274bdb109547a5f641f425c78396
Oliver Loescher <48457872+Olstar72@users.noreply.github.com>
2019-08-09 16:36:35 -0400

    Updateds

--- JohnChick/Assets/Scripts/Shooting.cs
+++ JohnChick/Assets/Scripts/Shooting.cs
@@ -7,8 +7,10 @@ public class Shooting : MonoBehaviour
 {
     [Header("Shooting")]
     [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private Transform muzzle;
+    [SerializeField] private List<Transform> muzzle = new List<Transform>();
     [SerializeField] private float forwardForce;
+
+    private int curMuzzle = 0;
 
     [Header("Bullet")]
     [SerializeField] private float bulletsPerSec;
@@ -33,14 +35,18 @@ public class Shooting : MonoBehaviour
         while (true)
         {
             GameObject bullet = Instantiate(bulletPrefab);
-            bullet.transform.position = muzzle.position;
-            bullet.transform.forward = muzzle.forward;
+            bullet.transform.position = muzzle[curMuzzle].position;
+            bullet.transform.forward = muzzle[curMuzzle].forward;
             bullet.transform.localScale *= bulletSize;
 
             bullet.GetComponent<Rigidbody>().AddForce(new Vector3(transform.forward.x * forwardForce, 0, transform.forward.z * forwardForce), ForceMode.Impulse);
             Destroy(bullet, bulletLife);
 
             CameraShaker.Instance.ShakeOnce(1, 2, 0.1f, 0.15f);
+
+            curMuzzle++;
+            if (curMuzzle == muzzle.Count)
+                curMuzzle = 0;
 
             yield return new WaitForSeconds(1f / bulletsPerSec);
         }
