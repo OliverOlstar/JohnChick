d661d42b76f9b248e0f09c26107a24653c1326f1
Mandalore477 <43152684+Mandalore477@users.noreply.github.com>
2019-08-09 16:08:26 -0400

    did something

--- JohnChick/Assets/Scripts/HighlightObject.cs
+++ JohnChick/Assets/Scripts/HighlightObject.cs
@@ -6,21 +6,43 @@ public class HighlightObject : MonoBehaviour
     public float animationTime = 1f;
     public float threshold = 1.5f;
 
+    private FastAndSlowEffect fastSlowEffect;
     private HighlightController controller;
     private Material material;
     private Color normalColor;
     private Color selectedColor;
+    private Color fastColor;
+    private Color slowColor;
     
     void Awake()
     {
         material = GetComponent<MeshRenderer>().material;
+        fastSlowEffect = GetComponent<FastAndSlowEffect>();
 
         normalColor = material.color;
         selectedColor = new Color(
             Mathf.Clamp01(normalColor.r * threshold),
             Mathf.Clamp01(normalColor.g * threshold),
             Mathf.Clamp01(normalColor.b * threshold));
+        fastColor = new Color(
+            Mathf.Clamp01(normalColor.r * (threshold * 2)),
+            Mathf.Clamp01(normalColor.g * (threshold * -1 / 2)),
+            Mathf.Clamp01(normalColor.b * (threshold * -1)));
+        slowColor = new Color(
+            Mathf.Clamp01(normalColor.r * (threshold * -1)),
+            Mathf.Clamp01(normalColor.g * threshold),
+            Mathf.Clamp01(normalColor.b * (threshold * 2)));
         controller = FindObjectOfType<HighlightController>();
+    }
+
+    void Update()
+    {
+        if (fastSlowEffect.timeScale > 1)
+            FastHighlight();
+        if (fastSlowEffect.timeScale < 1)
+            SlowHighlight();
+        if (fastSlowEffect.timeScale == 1)
+            StopHighlight();
     }
 
     public void StartHighlight()
@@ -41,6 +63,24 @@ public class HighlightObject : MonoBehaviour
     private void OnMouseOver()
     {
         controller.SelectObj(this);
+    }
+
+    public void FastHighlight()
+    {
+        iTween.ColorTo(gameObject, iTween.Hash(
+                "color", fastColor,
+                "time", (animationTime * 0.5),
+                "easetype", iTween.EaseType.linear,
+                "looptype", iTween.LoopType.pingPong));
+    }
+
+    public void SlowHighlight()
+    {
+        iTween.ColorTo(gameObject, iTween.Hash(
+                "color", slowColor,
+                "time", (animationTime),
+                "easetype", iTween.EaseType.linear,
+                "looptype", iTween.LoopType.pingPong));
     }
 
 }
