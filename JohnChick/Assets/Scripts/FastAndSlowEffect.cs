cbdc5f1ba3b6b9a6a85db8c9d0f975daee1bee95
Oliver Loescher <48457872+Olstar72@users.noreply.github.com>
2019-08-09 10:34:54 -0400

    Started work on Player Gun Effects

---
+++ JohnChick/Assets/Scripts/FastAndSlowEffect.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FastAndSlowEffect : MonoBehaviour
+{
+    public float timeScale = 1f;
+
+    [Header("Fill in Applicable")]
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private MovingPlatform movingPlatform;
+    [SerializeField] private speedingPlatform speedingPlatform;
+
+    private Vector3 DefaultVelocity;
+    //private NavMeshAgent navMeshAgent;
+    //private MovingPlatform movingPlatform;
+    //private speedingPlatform speedingPlatform;
+
+    private void Start()
+    {
+        //if (rigidbody)
+        //{
+        //    DefaultVelocity = rigidbody.velocity;
+        //}
+
+        //if (navMeshAgent)
+        //{
+
+        //}
+
+        //if (speedingPlatform)
+        //{
+
+        //}
+
+        //if (movingPlatform)
+        //{
+
+        //}
+    }
+
+    public void NewTimeScale(float pTimeChange)
+    {
+        timeScale = Mathf.Max(timeScale + pTimeChange, 0);
+
+        //if (rigidbody)
+        //{
+        //    rigidbody.velocity = DefaultVelocity * 
+        //}
+
+        //if (navMeshAgent)
+        //{
+
+        //}
+
+        //if (speedingPlatform)
+        //{
+
+        //}
+
+        //if (movingPlatform)
+        //{
+
+        //}
+    }
+}
