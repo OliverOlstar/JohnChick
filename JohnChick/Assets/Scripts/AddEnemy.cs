b877e47a0671bef5c703fc231650527965866050
Oliver Loescher <48457872+Olstar72@users.noreply.github.com>
2019-08-09 22:18:29 -0400

    Ease of life

---
+++ JohnChick/Assets/Scripts/AddEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AddEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject myPrefab;
+    [SerializeField] private Transform patrolRoute;
+    
+    void Start()
+    {
+        GameObject prefab = Instantiate(myPrefab);
+
+        prefab.transform.position = transform.position;
+        prefab.transform.rotation = transform.rotation;
+
+        GetComponent<Follow>().target = prefab.transform;
+        GetComponentInChildren<HoppingMovement>().speedSourceNav = prefab.GetComponent<NavMeshAgent>();
+
+        NPCSimplePatrol npcSimplePatrol = prefab.GetComponent<NPCSimplePatrol>();
+        Waypoints[] route = patrolRoute.GetComponentsInChildren<Waypoints>();
+
+        npcSimplePatrol._patrolPoints = route;
+    }
+}
--- JohnChick/Assets/Scripts/Follow.cs
+++ JohnChick/Assets/Scripts/Follow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Follow : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    public Transform target;
 
     // Start is called before the first frame update
     void Start()
